60e11a9c6116dd88d099f5785a06e7d94d43149d
Bernarth <alberthbernarth75@gmail.com>
2022-10-19 00:17:02 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'brace-style': [
+            'error',
+            'stroustrup',
+            { 'allowSingleLine': true }
+        ]
+    }
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+# Solutions for 30-Days-Of-JavaScript
+This project uses lite-server to run.
+
+To run this project use ```npm run dev```
+
+To fix lint issues use ```npm run lint:fix```
+
+I suggest you to see the git history to understand better.
