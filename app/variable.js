0a314c58cdaedc047eee1f616e0e1b51d9bf886a
Bernarth <alberthbernarth75@gmail.com>
2022-10-19 00:45:24 -0400

    Add day 1 exercises

---
+++ app/variable.js
@@ -0,0 +1,39 @@
+// string
+let name = 'Alberth';
+// boolean
+let unemployed = false;
+// undefined
+let initial;
+// null
+let comments = null;
+// show all the values
+console.log(name, unemployed, initial, comments);
+
+// data types
+console.log(typeof name, typeof unemployed, typeof initial, typeof comments);
+
+// Declare four variables without assigning values
+let color,
+    shape,
+    size,
+    mood;
+
+console.log(color, shape, size, mood);
+
+// Declare four variables with assigned values
+let up = 12,
+    down = 13,
+    left = 14,
+    right = 15;
+
+console.log(up, down, left, right);
+
+// first name, martial status, country and age
+let firstName = 'Alberth', martialStatus = false, country = 'Bolivia', age = 24;
+console.log(firstName, martialStatus, country, age);
+
+// log values
+let myAge = 24,
+    yourAge = 25;
+
+console.log(`I am ${myAge} years old.\nYou are ${yourAge} years old.`);
