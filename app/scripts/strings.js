4da8efaf32632d11db38f3579b30e4306e6a1de0
Bernarth <alberthbernarth75@gmail.com>
2022-11-01 17:06:09 -0400

    Add day 2 exercises level 3

--- app/scripts/strings.js
+++ app/scripts/strings.js
@@ -109,3 +109,30 @@ const phrase = 'You cannot end a sentence with because because because is a conj
 const because = 'because';
 const firstBecause = phrase.indexOf(because);
 console.log(phrase.substring(firstBecause, firstBecause + (because.length * 3) + 2));
+
+// Exercise: Level 3
+sentence = 'Love is the best thing in this world. Some found their love and some are still ' +
+    'looking for their love.';
+console.log((sentence.match(/love|Love/g) || []).length);
+
+sentence = 'You cannot end a sentence with because because because is a conjunction';
+console.log((sentence.match(/because/g) || []).length);
+
+sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; ' +
+    '&as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching ' +
+    'm%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? ' +
+    '%Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+sentence = sentence.replace(/[^a-zA-Z' ']/g, '');
+console.log(sentence);
+
+sentence = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, ' +
+    '15000 euro online courses per month.';
+sentence = sentence.replace(/[^0-9' ']/g, '');
+sentence = sentence.trim();
+let amounts = sentence.split(' ')
+    .filter(amount => amount.trim() !== '')
+    .map(amount => Number.parseInt(amount.trim()));
+console.log(amounts);
+let amount = amounts.reduce((acc, income) =>
+    acc + income, 0);
+console.log(amount);
