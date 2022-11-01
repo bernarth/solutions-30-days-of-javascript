9be09ab77cae6811d0f6c9d857607a7483600cd2
Bernarth <alberthbernarth75@gmail.com>
2022-11-01 16:31:38 -0400

    Add day 2 exercises level 2

--- app/scripts/strings.js
+++ app/scripts/strings.js
@@ -56,3 +56,56 @@ console.log('30 Days Of'.concat(' ', 'JavaScript'));
 
 console.log(challenge.repeat(2));
 
+// Exercise: Level 2
+console.log('The quote \'There is no exercise better for the heart than reaching down and' +
+    ' lifting people up.\' by John Holmes teaches us to help one another.');
+
+console.log('"Love is not patronizing and charity isn\'t about pity, it is about love. ' +
+    'Charity and love are the same -- with charity you give love, so don\'t just give ' +
+    'money but reach out your hand instead."');
+
+const type = 'number';
+let number = '10';
+if (typeof number !== type) {
+    console.log(Number.parseInt(number));
+}
+
+number = '9.8';
+console.log(Number.parseFloat(number));
+if (Number.parseFloat(number) !== 10) {
+    console.log(Math.round(Number.parseFloat(number)));
+}
+
+const words = ['python', 'jargon'];
+const responseWords = words.map(word => {
+    const contains = word.includes('on'),
+        response = ' contains word on';
+
+    return contains ?
+        word.concat(response) : word.concat(' doesn\'t', response);
+});
+console.log(responseWords);
+
+sentence = 'I hope this course is not full of jargon';
+console.log(sentence.includes('jargon'));
+
+// Math.random generates numbers from 0 to 1 without ever reaching 1
+console.log(Math.floor(Math.random() * 101));
+
+console.log(Math.floor(Math.random() * 51) + 50);
+
+console.log(Math.floor(Math.random() * 256));
+
+const language = 'JavaScript';
+console.log(language[Math.floor(Math.random() * language.length)]);
+
+console.log('1 1 1 1 1\n' +
+    '2 1 2 4 8\n' +
+    '3 1 3 9 27\n' +
+    '4 1 4 16 64\n' +
+    '5 1 5 25 125');
+
+const phrase = 'You cannot end a sentence with because because because is a conjunction';
+const because = 'because';
+const firstBecause = phrase.indexOf(because);
+console.log(phrase.substring(firstBecause, firstBecause + (because.length * 3) + 2));
